c9a6cad9de1f8b3145c4797f23f46b57bdb87694
Norasmadi AR <norasmadi@gmail.com>
2022-11-22 23:33:10 +0800

    Update 05serial16x2_LCD.py

--- Basic_Kits/05 Serial LCD/05serial16x2_LCD.py
+++ Basic_Kits/05 Serial LCD/05serial16x2_LCD.py
@@ -1,5 +1,11 @@
 '''
-https://github.com/Guitarman9119/Raspberry-Pi-Pico-/tree/main/I2C%2016x2%20LCD%20Circuit%20Python
+Example for Pi Pico W. I2C LCD
+References:
+  [1] https://github.com/Guitarman9119/Raspberry-Pi-Pico-/tree/main/I2C%2016x2%20LCD%20Circuit%20Python
+
+Additional Library:
+- lcd.py from [1]
+- i2c_pcf8574_interface.py from [1]
 '''
 import board
 import time
