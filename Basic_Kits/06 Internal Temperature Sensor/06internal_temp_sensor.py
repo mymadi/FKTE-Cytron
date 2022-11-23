9b6c091f7291d48738f3f8a478e611a150def6ad
Norasmadi AR <norasmadi@gmail.com>
2022-11-23 20:12:35 +0800

    Create 06internal_temp_sensor.py

---
+++ Basic_Kits/06 Internal Temperature Sensor/06internal_temp_sensor.py
@@ -0,0 +1,15 @@
+'''
+The Raspberry Pi Pico W's CPU has a
+temperature sensor built into it.
+CircuitPython makes it super simple to read
+this data from the sensor using the microcontroller module
+'''
+
+import time
+import microcontroller
+
+while True:
+    data = microcontroller.cpu.temperature
+    data = "{:.2f}".format(data)
+    print("Temperature: ", data, "celcius")
+    time.sleep(1)
