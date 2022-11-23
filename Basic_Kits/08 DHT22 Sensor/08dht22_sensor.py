a1316ffee25a7ba0e11834c0142022e18474acf6
Norasmadi AR <norasmadi@gmail.com>
2022-11-23 21:44:10 +0800

    DHT22 Sensor

---
+++ Basic_Kits/08 DHT22 Sensor/08dht22_sensor.py
@@ -0,0 +1,28 @@
+'''
+DHT22 Sensor: Temperature and Humidity
+Additional Library:
+ - adafruit_dht
+ '''
+
+import time
+import board
+import adafruit_dht
+
+dht22 = adafruit_dht.DHT22(board.GP7)
+
+while True:
+    try:
+        temperature = dht22.temperature
+        humidity = dht22.humidity
+        
+        print("Temperature: {:.2f} °C, Humidity: {:.2f} %RH ".format(temperature, humidity))
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dht22.exit()
+        raise error
+
+    time.sleep(2.0)
