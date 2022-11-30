da9e45971e9c9a82f8f6255638c3299f2ad18437
Norasmadi AR <norasmadi@gmail.com>
2022-11-30 20:12:10 +0800

    Create 15ultrasonic_hcsr04.py

---
+++ Basic_Kits/15 3V-5.5V SR04P Ultrasonic Ranging Module/15ultrasonic_hcsr04.py
@@ -0,0 +1,21 @@
+'''
+Example Pi Pico W. 3V-5.5V SR04P Ultrasonic Ranging Module.
+
+Additional Library:
+    - adafruit_hcsr04.py
+'''
+
+import time
+import board
+import adafruit_hcsr04
+
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP7, echo_pin=board.GP6)
+
+
+while True:
+    try:
+        print(sonar.distance)
+    except RuntimeError:
+        print("Retrying!")
+        pass
+    time.sleep(0.1)
