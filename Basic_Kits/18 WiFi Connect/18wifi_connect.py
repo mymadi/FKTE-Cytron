a9581aef590774592cc141d59e9660d5b75ffc43
Norasmadi AR <norasmadi@gmail.com>
2022-12-06 20:40:29 +0800

    Basic WiFi Connect

---
+++ Basic_Kits/18 WiFi Connect/18wifi_connect.py
@@ -0,0 +1,37 @@
+'''
+Test Basic WiFi for Pi Pico W
+
+How to create .env file?
+https://learn.adafruit.com/pico-w-wifi-with-circuitpython/create-your-environmental-variables-env-file
+'''
+
+import os
+import ipaddress
+import wifi
+import socketpool
+
+print(os.getenv('test_env_file'))
+
+ssid = os.getenv('WIFI_SSID')
+password = os.getenv('WIFI_PASSWORD')
+
+print("Connecting to WiFi '{}' ... ".format(ssid), end="")
+
+#  Connect to Wi-Fi AP
+wifi.radio.connect(ssid, password)
+
+print("connected!")
+print()
+
+pool = socketpool.SocketPool(wifi.radio)
+
+#  Print MAC address
+print("Pico W MAC address: ", [hex(i) for i in wifi.radio.mac_address])
+
+#  Print IP address
+print("Pico W IP address: ", wifi.radio.ipv4_address)
+print()
+
+#  Ping to google.com
+ipv4 = ipaddress.ip_address("8.8.4.4")
+print("Ping to google.com: %f ms" % (wifi.radio.ping(ipv4)*1000))
