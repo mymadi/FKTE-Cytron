91fad52af59bc7d35ce16b75f7e8954b07dee277
Norasmadi AR <norasmadi@gmail.com>
2022-12-09 21:35:39 +0800

    Simple Home Environment Monitoring System

--- Projects/01P Simple Home Environment Monitoring System/01P_SHEMS.py
+++ Projects/01P Simple Home Environment Monitoring System/01P_SHEMS.py
@@ -8,9 +8,9 @@ References:
   [1] https://github.com/Guitarman9119/Raspberry-Pi-Pico-/tree/main/I2C%2016x2%20LCD%20Circuit%20Python
 
 Additional Library:
-- lcd.py from [1]
-- i2c_pcf8574_interface.py from [1]
-- adafruit_dht.mpy
+  - lcd.py from [1]
+  - i2c_pcf8574_interface.py from [1]
+  - adafruit_dht.mpy
 '''
 import board
 import time
