5a508ec9f9b469991c9fa45fb53dd7cbdf73340f
Norasmadi AR <norasmadi@gmail.com>
2023-03-25 07:32:48 +0800

    Basic Robot Movements

--- Robotic_Kits/01 Basic Robot Movements/01BasicRobotMovement.py
+++ Robotic_Kits/01 Basic Robot Movements/01BasicRobotMovement.py
@@ -1,5 +1,5 @@
 '''
-Basic Robot Movement using Maker Drive and CircuitPython on Pi Pico W.
+Basic Robot Movement using Maker Drive and CircuitPython on Pi Pico W
 
 Libraries required from bundle (https://circuitpython.org/libraries):
   - adafruit_motor
