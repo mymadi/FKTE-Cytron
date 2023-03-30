93de960a4e5babe7ce59bfa2776f8d420d489c8e
Norasmadi AR <norasmadi@gmail.com>
2023-03-30 20:12:20 +0800

    Basic Robot Movements

--- Robotic_Kits/01 Basic Robot Movements/01RobotMovement.py
+++ Robotic_Kits/01 Basic Robot Movements/01RobotMovement.py
@@ -1,5 +1,6 @@
 '''
-Basic Robot Movement using Maker Drive and CircuitPython on Pi Pico W
+Basic Robot Movement using Maker Drive and CircuitPython on Pi Pico W.
+
 Libraries required from bundle (https://circuitpython.org/libraries):
   - adafruit_motor
   
@@ -15,22 +16,22 @@ import digitalio
 import pwmio
 from adafruit_motor import motor
 
-# Right Motor
-PWM_M1A = board.GP14
-PWM_M1B = board.GP15
 # Left Motor
-PWM_M2A = board.GP17
-PWM_M2B = board.GP16
+PWM_M1A = board.GP16
+PWM_M1B = board.GP17
+# Right Motor
+PWM_M2A = board.GP14
+PWM_M2B = board.GP15
 
 # DC motor setup
 # DC Motors generate electrical noise when running that can reset the microcontroller in extreme
 # cases. A capacitor can be used to help prevent this.
 pwm_1a = pwmio.PWMOut(PWM_M1A, frequency=10000)
 pwm_1b = pwmio.PWMOut(PWM_M1B, frequency=10000)
-motorR = motor.DCMotor(pwm_1a, pwm_1b)
+motorL = motor.DCMotor(pwm_1a, pwm_1b)
 pwm_2a = pwmio.PWMOut(PWM_M2A, frequency=10000)
 pwm_2b = pwmio.PWMOut(PWM_M2B, frequency=10000)
-motorL = motor.DCMotor(pwm_2a, pwm_2b)
+motorR = motor.DCMotor(pwm_2a, pwm_2b)
 
 def Robot_Stop():
     motorL.throttle = 0
