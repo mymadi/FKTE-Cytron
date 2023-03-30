edabcfa5d460b3646f84ff8a3148d69288ef6bf0
Norasmadi AR <norasmadi@gmail.com>
2023-03-30 20:44:51 +0800

    Update 02Obstacleavoidance.py

--- Robotic_Kits/02 Obstacle Avoidance/02Obstacleavoidance.py
+++ Robotic_Kits/02 Obstacle Avoidance/02Obstacleavoidance.py
@@ -15,22 +15,22 @@ import adafruit_hcsr04
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP13, echo_pin=board.GP12)
 
-# Right Motor
-PWM_M1A = board.GP14
-PWM_M1B = board.GP15
 # Left Motor
-PWM_M2A = board.GP17
-PWM_M2B = board.GP16
+PWM_M1A = board.GP16
+PWM_M1B = board.GP17
+# Right Motor
+PWM_M2A = board.GP14
+PWM_M2B = board.GP15
 
 # DC motor setup
 # DC Motors generate electrical noise when running that can reset the microcontroller in extreme
 # cases. A capacitor can be used to help prevent this.
 pwm_1a = pwmio.PWMOut(PWM_M1A, frequency=10000)
 pwm_1b = pwmio.PWMOut(PWM_M1B, frequency=10000)
-motorR = motor.DCMotor(pwm_1a, pwm_1b)
+motorL = motor.DCMotor(pwm_1a, pwm_1b)
 pwm_2a = pwmio.PWMOut(PWM_M2A, frequency=10000)
 pwm_2b = pwmio.PWMOut(PWM_M2B, frequency=10000)
-motorL = motor.DCMotor(pwm_2a, pwm_2b)
+motorR = motor.DCMotor(pwm_2a, pwm_2b)
 
 def Robot_Stop():
     motorL.throttle = 0
