5bcf8491a3f2b924ab9a59f21117242476c2d37a
Norasmadi AR <norasmadi@gmail.com>
2023-04-16 06:07:17 +0800

    Update 18wifi_connect.py

--- Basic_Kits/18 WiFi Connect/18wifi_connect.py
+++ Basic_Kits/18 WiFi Connect/18wifi_connect.py
@@ -1,8 +1,8 @@
 '''
 Test Basic WiFi for Pi Pico W
 
-How to create .env file?
-https://learn.adafruit.com/pico-w-wifi-with-circuitpython/create-your-environmental-variables-env-file
+How to create environment file?
+https://docs.circuitpython.org/en/latest/docs/environment.html
 '''
 
 import os
