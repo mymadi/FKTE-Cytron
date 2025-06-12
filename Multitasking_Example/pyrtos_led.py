5d768807da979946742810018e743c7ebae4bb8e
Norasmadi AR <norasmadi@gmail.com>
2025-06-12 22:29:04 +0800

    Update pyrtos_led.py

--- Multitasking_Example/pyrtos_led.py
+++ Multitasking_Example/pyrtos_led.py
@@ -1,46 +1,61 @@
 '''
-pyRTOS (Real Time Operating Systems)
+Project: Concurrent Task Management with pyRTOS
 
-Additional Library:
-    - pyRTOS
+Description:
+This script demonstrates the power of the pyRTOS library
+for cooperative multitasking on the Raspberry Pi Pico W. It sets up
+multiple independent tasks that run concurrently:
+- 16 individual LEDs blinking at different rates.
+- A push button monitoring task (sw17) that toggles an LED (GP16)
+  and updates a global boolean variable (swT).
+- A random data generation task (RD18) that updates a global float variable (RData).
+- The status of swT is printed by LED1 Task (GP0).
+- The value of RData is printed by LED2 Task (GP1).
 
-Where to gets the Library?
+Library Used:
+    - pyRTOS: https://github.com/Rybec/pyRTOS
 
-https://github.com/Rybec/pyRTOS/tree/main/pyRTOS
+Installation Note:
+    Download the pyRTOS library from the link above.
+    Copy the entire 'pyRTOS' folder (containing __init__.py, scheduler.py, etc.)
+    into the 'lib' directory on your CircuitPython device's CIRCUITPY drive:
+    e.g., CIRCUITPY/lib/pyRTOS/
 
-Note:
-
-1) Push Button, GP21 (sw17 task) status will be printed at led1 Task
-2) Random Data (RD18 Task) value will be printed at led2 Task
-
-Reference:
-
-1) https://www.seeedstudio.com/blog/2021/04/26/rtos-basics-getting-started-with-microcontrollers/
-2) https://my.cytron.io/tutorial/real-time-multitasking-on-maker-pi-pico-using-pyrtos
-3) https://my.cytron.io/tutorial/real-time-iot-room-monitoring-on-maker-pi-pico-using-pyrtos
+References:
+1) RTOS Basics: Getting Started with Microcontrollers: https://www.seeedstudio.com/blog/2021/04/26/rtos-basics-getting-started-with-microcontrollers/
+2) Real-time Multitasking on Maker Pi Pico Using pyRTOS: https://my.cytron.io/tutorial/real-time-multitasking-on-maker-pi-pico-using-pyrtos
+3) Real-time IoT Room Monitoring on Maker Pi Pico Using pyRTOS: https://my.cytron.io/tutorial/real-time-iot-room-monitoring-on-maker-pi-pico-using-pyrtos
 '''
 
 import board
 import digitalio
 import time
-import pyRTOS
+import pyRTOS # Import the pyRTOS library (Rybec/pyRTOS)
 import random
 
-swT = False # Initialize global variables
-RData = 0   # Initialize global variables
+# --- Global Variables for Inter-Task Communication ---
+# These variables allow tasks to share data, but be mindful of race conditions
+# in more complex scenarios (pyRTOS's Message system is generally safer).
+swT = False   # Global status for the button (True/False)
+RData = 0.0   # Global variable to store random data
+
+# --- Task Definitions ---
+# Each task is a generator function that takes 'self' as its first argument.
 
 def led1(self):
+    """Controls LED on GP0, blinks and prints swT status."""
     ledpin1 = digitalio.DigitalInOut(board.GP0)
     ledpin1.direction = digitalio.Direction.OUTPUT
-    ledpin1.value = False
-    yield
+    ledpin1.value = False # Ensure LED is off initially
+    yield # Initial yield to allow setup to complete before loop
 
     while True:
-        ledpin1.value = not ledpin1.value
-        print("LED1 Task",swT)
-        yield [pyRTOS.timeout(1)]
+        ledpin1.value = not ledpin1.value # Toggle LED state
+        print(f"LED1 Task (GP0): swT status = {swT}") # Access global swT
+        yield [pyRTOS.timeout(1)] # Yield control for 1 second
 
 def led2(self):
+    """Controls LED on GP1, blinks and prints RData value."""
     ledpin2 = digitalio.DigitalInOut(board.GP1)
     ledpin2.direction = digitalio.Direction.OUTPUT
     ledpin2.value = False
@@ -48,10 +63,11 @@ def led2(self):
 
     while True:
         ledpin2.value = not ledpin2.value
-        print("LED2 Task",RData)
+        print(f"LED2 Task (GP1): Random Data = {RData:.2f}") # Access global RData
         yield [pyRTOS.timeout(0.95)]
 
 def led3(self):
+    """Controls LED on GP2, blinks at 0.90s."""
     ledpin3 = digitalio.DigitalInOut(board.GP2)
     ledpin3.direction = digitalio.Direction.OUTPUT
     ledpin3.value = False
@@ -62,6 +78,7 @@ def led3(self):
         yield [pyRTOS.timeout(0.90)]
 
 def led4(self):
+    """Controls LED on GP3, blinks at 0.85s."""
     ledpin4 = digitalio.DigitalInOut(board.GP3)
     ledpin4.direction = digitalio.Direction.OUTPUT
     ledpin4.value = False
@@ -72,6 +89,7 @@ def led4(self):
         yield [pyRTOS.timeout(0.85)]
 
 def led5(self):
+    """Controls LED on GP4, blinks at 0.80s."""
     ledpin5 = digitalio.DigitalInOut(board.GP4)
     ledpin5.direction = digitalio.Direction.OUTPUT
     ledpin5.value = False
@@ -82,6 +100,7 @@ def led5(self):
         yield [pyRTOS.timeout(0.80)]
 
 def led6(self):
+    """Controls LED on GP5, blinks at 0.75s."""
     ledpin6 = digitalio.DigitalInOut(board.GP5)
     ledpin6.direction = digitalio.Direction.OUTPUT
     ledpin6.value = False
@@ -92,6 +111,7 @@ def led6(self):
         yield [pyRTOS.timeout(0.75)]
 
 def led7(self):
+    """Controls LED on GP6, blinks at 0.70s."""
     ledpin7 = digitalio.DigitalInOut(board.GP6)
     ledpin7.direction = digitalio.Direction.OUTPUT
     ledpin7.value = False
@@ -102,6 +122,7 @@ def led7(self):
         yield [pyRTOS.timeout(0.70)]
 
 def led8(self):
+    """Controls LED on GP7, blinks at 0.65s."""
     ledpin8 = digitalio.DigitalInOut(board.GP7)
     ledpin8.direction = digitalio.Direction.OUTPUT
     ledpin8.value = False
@@ -112,6 +133,7 @@ def led8(self):
         yield [pyRTOS.timeout(0.65)]
 
 def led9(self):
+    """Controls LED on GP8, blinks at 0.60s."""
     ledpin9 = digitalio.DigitalInOut(board.GP8)
     ledpin9.direction = digitalio.Direction.OUTPUT
     ledpin9.value = False
@@ -122,6 +144,7 @@ def led9(self):
         yield [pyRTOS.timeout(0.60)]
 
 def led10(self):
+    """Controls LED on GP9, blinks at 0.55s."""
     ledpin10 = digitalio.DigitalInOut(board.GP9)
     ledpin10.direction = digitalio.Direction.OUTPUT
     ledpin10.value = False
@@ -132,6 +155,7 @@ def led10(self):
         yield [pyRTOS.timeout(0.55)]
 
 def led11(self):
+    """Controls LED on GP10, blinks at 0.50s."""
     ledpin11 = digitalio.DigitalInOut(board.GP10)
     ledpin11.direction = digitalio.Direction.OUTPUT
     ledpin11.value = False
@@ -142,6 +166,7 @@ def led11(self):
         yield [pyRTOS.timeout(0.50)]
 
 def led12(self):
+    """Controls LED on GP11, blinks at 0.45s."""
     ledpin12 = digitalio.DigitalInOut(board.GP11)
     ledpin12.direction = digitalio.Direction.OUTPUT
     ledpin12.value = False
@@ -152,6 +177,7 @@ def led12(self):
         yield [pyRTOS.timeout(0.45)]
 
 def led13(self):
+    """Controls LED on GP12, blinks at 0.40s."""
     ledpin13 = digitalio.DigitalInOut(board.GP12)
     ledpin13.direction = digitalio.Direction.OUTPUT
     ledpin13.value = False
@@ -162,6 +188,7 @@ def led13(self):
         yield [pyRTOS.timeout(0.40)]
 
 def led14(self):
+    """Controls LED on GP13, blinks at 0.35s."""
     ledpin14 = digitalio.DigitalInOut(board.GP13)
     ledpin14.direction = digitalio.Direction.OUTPUT
     ledpin14.value = False
@@ -172,6 +199,7 @@ def led14(self):
         yield [pyRTOS.timeout(0.35)]
 
 def led15(self):
+    """Controls LED on GP14, blinks at 0.30s."""
     ledpin15 = digitalio.DigitalInOut(board.GP14)
     ledpin15.direction = digitalio.Direction.OUTPUT
     ledpin15.value = False
@@ -182,6 +210,7 @@ def led15(self):
         yield [pyRTOS.timeout(0.30)]
 
 def led16(self):
+    """Controls LED on GP15, blinks at 0.25s."""
     ledpin16 = digitalio.DigitalInOut(board.GP15)
     ledpin16.direction = digitalio.Direction.OUTPUT
     ledpin16.value = False
@@ -192,45 +221,61 @@ def led16(self):
         yield [pyRTOS.timeout(0.25)]
 
 def sw17(self):
-    global swT
+    """Monitors push button (GP21) and updates global swT status, controls LED on GP16."""
+    global swT # Declare intent to modify the global variable
     swpin17 = digitalio.DigitalInOut(board.GP21)
     swpin17.direction = digitalio.Direction.INPUT
+    # Assuming internal pull-up/down is managed by Maker Pi Pico board or external resistor
+    # If not, add swpin17.pull = digitalio.Pull.UP or DOWN if needed.
+
     ledpin17 = digitalio.DigitalInOut(board.GP16)
     ledpin17.direction = digitalio.Direction.OUTPUT
-    ledpin17.value = False
+    ledpin17.value = False # LED off initially
     yield
 
     while True:
+        # Check if button is pressed (assuming active low, i.e., False when pressed)
         if swpin17.value == False:
-            ledpin17.value = not ledpin17.value
-            swT = not swT
-        yield [pyRTOS.timeout(0.15)]
-        
+            ledpin17.value = not ledpin17.value # Toggle LED on GP16
+            swT = not swT # Toggle global switch status
+        yield [pyRTOS.timeout(0.15)] # Check button every 0.15 seconds (debounce)
+
 def RD18(self):
-    global RData
-    yield
+    """Generates random data and updates global RData variable."""
+    global RData # Declare intent to modify the global variable
+    yield # Initial yield
 
     while True:
-        RData = random.uniform(0,250)
-        yield [pyRTOS.timeout(1)]
-        
-pyRTOS.add_task(pyRTOS.Task(led1, name="1"))
-pyRTOS.add_task(pyRTOS.Task(led2, name="2"))
-pyRTOS.add_task(pyRTOS.Task(led3, name="3"))
-pyRTOS.add_task(pyRTOS.Task(led4, name="4"))
-pyRTOS.add_task(pyRTOS.Task(led5, name="5"))
-pyRTOS.add_task(pyRTOS.Task(led6, name="6"))
-pyRTOS.add_task(pyRTOS.Task(led7, name="7"))
-pyRTOS.add_task(pyRTOS.Task(led8, name="8"))
-pyRTOS.add_task(pyRTOS.Task(led9, name="9"))
-pyRTOS.add_task(pyRTOS.Task(led10, name="10"))
-pyRTOS.add_task(pyRTOS.Task(led11, name="11"))
-pyRTOS.add_task(pyRTOS.Task(led12, name="12"))
-pyRTOS.add_task(pyRTOS.Task(led13, name="13"))
-pyRTOS.add_task(pyRTOS.Task(led14, name="14"))
-pyRTOS.add_task(pyRTOS.Task(led15, name="15"))
-pyRTOS.add_task(pyRTOS.Task(led16, name="16"))
-pyRTOS.add_task(pyRTOS.Task(sw17, name="17"))
-pyRTOS.add_task(pyRTOS.Task(RD18, name="18"))
+        RData = random.uniform(0, 250) # Generate a random float between 0 and 250
+        yield [pyRTOS.timeout(1)] # Update random data every 1 second
 
+# --- RTOS Setup ---
+print("Adding tasks to pyRTOS scheduler...")
+
+# Create Task instances and add them to the scheduler
+# pyRTOS.Task(task_function, name=None, priority=0, mailbox=False)
+pyRTOS.add_task(pyRTOS.Task(led1, name="LED_GP0", priority=1))
+pyRTOS.add_task(pyRTOS.Task(led2, name="LED_GP1", priority=2))
+pyRTOS.add_task(pyRTOS.Task(led3, name="LED_GP2", priority=3))
+pyRTOS.add_task(pyRTOS.Task(led4, name="LED_GP3", priority=4))
+pyRTOS.add_task(pyRTOS.Task(led5, name="LED_GP4", priority=5))
+pyRTOS.add_task(pyRTOS.Task(led6, name="LED_GP5", priority=6))
+pyRTOS.add_task(pyRTOS.Task(led7, name="LED_GP6", priority=7))
+pyRTOS.add_task(pyRTOS.Task(led8, name="LED_GP7", priority=8))
+pyRTOS.add_task(pyRTOS.Task(led9, name="LED_GP8", priority=9))
+pyRTOS.add_task(pyRTOS.Task(led10, name="LED_GP9", priority=10))
+pyRTOS.add_task(pyRTOS.Task(led11, name="LED_GP10", priority=11))
+pyRTOS.add_task(pyRTOS.Task(led12, name="LED_GP11", priority=12))
+pyRTOS.add_task(pyRTOS.Task(led13, name="LED_GP12", priority=13))
+pyRTOS.add_task(pyRTOS.Task(led14, name="LED_GP13", priority=14))
+pyRTOS.add_task(pyRTOS.Task(led15, name="LED_GP14", priority=15))
+pyRTOS.add_task(pyRTOS.Task(led16, name="LED_GP15", priority=16))
+pyRTOS.add_task(pyRTOS.Task(sw17, name="Button_GP21", priority=17)) # Button task
+pyRTOS.add_task(pyRTOS.Task(RD18, name="Random_Data", priority=18)) # Random data task
+
+print("Starting pyRTOS scheduler...")
+# Start the RTOS scheduler. For Rybec/pyRTOS, no arguments are passed here.
 pyRTOS.start()
+
+# This line will generally not be reached, as the RTOS scheduler runs indefinitely.
+print("Scheduler stopped. (This message implies all tasks have terminated, which is unlikely for this code)")
